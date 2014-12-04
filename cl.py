0568851245eae5022762693540c914a0dc4b80da
Juan Orlandini <tachijuan@gmail.com>
2014-12-04 15:45:03 -0500

    a sample command line argv test

---
+++ cl.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import sys
+
+if len(sys.argv) < 2:
+  print " You need to enter more one or more files as the arguments to this script"
+  exit(42)
+else: 
+  for myfile in sys.argv:
+    print myfile
+
