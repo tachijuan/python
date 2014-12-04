e5a096bb7f45fbf5b2a05e9eef84347398f3be90
Juan Orlandini <juan@orlandini.us>
2014-12-04 15:23:08 -0500

    Fixed comments and removed id from compare

--- comparetwolists.py
+++ comparetwolists.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
 
-import prettytable
+import prettytable                  # this lets us print it in a ... pretty table.
 
 
 a=open('ina').read().splitlines()    # read file "ina" into list a
 b=open('ine').read().splitlines()    # read file "ine" into list b
 c=list(set(a+b))                     # combine the two list and identify the unique set members, convert to list c
 c.sort()                             # sort c
-e = prettytable.PrettyTable(['client','Aptare','Equifax'])    # setup a nice table with headers
+e = prettytable.PrettyTable(['client','Report','Customer'])    # setup a nice table with headers
 for x in c:                          # iterate through each member of x
      ina = ine = " "                 # default to not in either
      if x in a: ina = "x"            # table entry for "ina"
